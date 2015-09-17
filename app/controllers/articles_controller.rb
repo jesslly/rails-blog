55807f68ab36d49e581339af0a06d4055b6f9312
JellyWong <jesslly.wong@monexa.com>
2015-09-17 15:31:57 -0700

    first initial check in

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,55 @@
+class ArticlesController < ApplicationController
+	def index
+		@articles = Article.all
+	end
+
+	def show
+
+		logger.debug "Show article: #{params}"
+
+		@article = Article.find(params[:id])
+	end
+
+	def new
+		@article = Article.new
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render 'edit'
+		end
+	end
+
+	def create
+		@article = Article.new(article_params)
+
+		if @article.save
+			redirect_to @article
+		else
+			render 'new'
+		end
+
+		logger.debug "New article saved: #{@article.attributes.inspect}"
+
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+
+		redirect_to articles_path
+	end
+
+	private
+		def article_params
+	  		params.require(:article).permit(:title, :text)
+		end
+end
