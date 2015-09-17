c0d2ea0fa4be977f7b18b250fe98107380fde0db
JellyWong <jesslly.wong@monexa.com>
2015-09-17 16:45:19 -0700

    finish with comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,21 @@
 class CommentsController < ApplicationController
+	def create
+		@article = Article.find(params[:article_id])
+		@comment = @article.comments.create(comment_params)
+		redirect_to article_path(@article)
+	end
+
+	def destroy
+		logger.debug "Delete article comment: #{params}"
+
+		@article = Article.find(params[:article_id])
+		@comment = @article.comments.find(params[:id])
+		@comment.destroy
+		redirect_to article_path(@article)
+	end
+
+	private 
+		def comment_params
+			params.require(:comment).permit(:commenter, :body)
+		end
 end
